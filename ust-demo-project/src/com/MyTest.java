46c7ea81e7bd0a92cc7bced8e0f3b08363fde218
ustjava18 <ustjava18@java37.iiht.tech>
2021-05-27 13:32:34 +0530

    ust demo project commited

---
+++ ust-demo-project/src/com/MyTest.java
@@ -0,0 +1,29 @@
+package com;
+
+public class MyTest {
+
+	public static void main(String[] args)
+    {
+  
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+  
+        // find the largest number
+        // using && operator
+        if (num1 >= num2 && num1 >= num3)
+            System.out.println(
+                num1
+                + " is the largest number.");
+        else if (num2 >= num1 && num2 >= num3)
+            System.out.println(
+                num2
+                + " is the largest number.");
+        else
+            System.out.println(
+                num3
+                + " is the largest number.");
+    }
+
+
+}
